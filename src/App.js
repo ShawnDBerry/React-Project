0320bb165d2313e6b499cb22935f7f729fcc6470
Monique Berry <MainFolder@mymacs-mbp.attlocal.net>
2018-03-06 17:22:04 -0600

    Person Css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,77 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      person: [
+        {
+          id:'dfgdfg',
+          name: 'Max',
+          age: 28
+        }, {
+          id: 'fbav',
+          name: 'Manu',
+          age: 29
+        }, {
+          id: 'asfgkr',
+          name: 'Stephanie',
+          age: 26
+        }
+      ],
+      showing: false
+    };
+  }
+
+  personList() {
+    const personsList = this
+      .state
+      .person
+      .map((person, index) => 
+      <div>
+        <Person name={person.name} age={person.age} click={() => this.deletePersonHandler(index)} key={person.id}></Person>
+      </div>);
+    return personsList;
+  }
+
+  deletePersonHandler = personIndex => {
+    const persons = [...this.state.person];
+    persons.splice(personIndex, 1);
+    this.setState({person: persons})
+  }
+
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
-       <h1>Hi, I'm a React App</h1>
-       <p>This is really working!</p>
-       <Person></Person>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button style={style} onClick={() => this.setState({showing: !this.state.showing})}>Click</button>
+        {this.state.showing
+          ? <div>
+              {this.personList()}
+              {/* <Person name={this.state.person[0].name} age={this.state.person[0].age}></Person>
+              <Person name={this.state.person[1].name} age={this.state.person[1].age}></Person>
+              <Person name={this.state.person[2].name} age={this.state.person[2].age}></Person> */}
+            </div>
+          : null
+}
+
       </div>
     );
-    //return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'), )
+    // return React.createElement('div', {className:'App'},
+    // React.createElement('h1', null, 'Hi, I\'m a React App!!!'), )
   }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
+import './Person.css';
 
-const person = () => {
-    return <p>Wassup</p>
+export default class Person extends Component {
+    constructor(props) {
+        super();
+    };
+
+    render() {
+
+        return(
+            <div className="Person">
+            <p onClick={this.props.click}>{this.props.name}</p>
+            <p>{this.props.age}</p>
+        </div>
+        );
+
+    }
+
 }
-
-export default person
